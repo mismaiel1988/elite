4a0028cd800860c0fc7d7de1b2446dd8100f293c
Maya Ismaiel <maya.ismaiel@gmail.com>
2016-08-31 07:53:15 -0700

    8/31

---
+++ index.php
@@ -0,0 +1,320 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<script type="text/javascript" src="http://www.youtube.com/player_api"></script>
+	<link rel="shortcut icon" href="fiveicon.png" type="image/png">
+</head>
+	<body>
+	<div class="pk">
+		<div id="videoitshelg" style="text-align:center">
+			<iframe id= "video1" width="100%;" src="https://www.youtube.com/embed/G_0l8nlnceE?enablejsapi=1" frameborder="0" allowfullscreen></iframe>
+		</div>
+		<div id="closevideorightnow"></div>
+	</div>
+	<div class="mobile">
+		<div class="mobilevideo">
+			<iframe class="videoiframemobile"  src="https://www.youtube.com/embed/G_0l8nlnceE?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</div>
+		<div class="topmenu">
+			<div class="logosection">
+			<div class="logohome"></div>
+
+			</div>
+			<div class="menusection">
+				<div class="menuitemswrap">
+					<a href="#section_home"><div class="menuitemhome">HOME</div></a>
+					<a href="#section_aboutus"><div class="menuitem">ABOUT</div></a>
+					<a href="#section_students"><div class="menuitem">STUDENTS</div></a>
+					<a href="#section_instructors"><div class="menuitem">INSTRUCTORS</div></a>
+					<a href="#section_features"><div class="menuitem">FEATURES</div></a>
+					<a href="#section_download"><div class="menuitem">DOWNLOAD</div></a>
+				</div>
+			</div>
+			<div class="mobilemenuhamburger"></div>
+		</div>
+		<div id="section_home" class="section_home">
+			<div class="homewrap">
+				<div class="herotext">A new way for students and instructors to connect!</div>
+				<div class="watchvideobtn">
+					Watch the video
+					<div class="pk"><button id="videobuttondawdaw" class="videobuttondawdaw"></button></div>
+					<div class="mobile"><button id="videobuttondawdaw123123" class="videobuttondawdaw" style="margin-top:20px;"></button></div>
+				</div>
+				<div>
+					<a href="https://itunes.apple.com/us/app/elite-instructors-student/id991247242?mt=8">
+					<img class="appstorebtm" src="img/app_store_button@2x.png">
+					</a>
+				</div>
+			</div>
+
+		</div>
+		<div id="section_aboutus" class="section_aboutus">
+			<div class="aboutustitle">Elite is creating more!</div>
+			<div class="aboutustext">Our mobile app allows users to select any topic of interest such as math, exam preparation, language, personal fitness, piano,
+				golf, etc. and book an in-person tutoring session with quality instructors at the convenience and comfort of their own home
+				and schedule. Our goal is to make learning fun by introducing the social element to personalized education.</div>
+			<div class="play-button">
+				<div class="aboutusimage"><img style="width:100px;height:auto;" src="img/play_button@2x.png"></div>
+				<div class="aboutusimage"><img style="width:100px;height:auto;" src="img/play_button@2x.png"></div>
+				<div class="aboutusimage"><img style="width:100px;height:auto;" src="img/play_button@2x.png"></div>
+				<div class="aboutusimage"><img style="width:100px;height:auto;" src="img/play_button@2x.png"></div>
+				<div class="aboutusimage"><img style="width:100px;height:auto;" src="img/play_button@2x.png"></div>
+				<div class="aboutusimage"><img style="width:100px;height:auto;" src="img/play_button@2x.png"></div>
+			</div>
+		</div>
+		<div id="section_students" class="section_students">
+			<div class="studenthalf"><img class="iphone-students" src="img/iPhone 6_mockup_students_white@2x.png"></div>
+			<div class="studenthalf">
+				<div class="studentstext1">For</div>
+				<div class="studentstext2">Students</div>
+				<div class="studentstext3">Individuals with different learning abilities and varied comprehension strengths will benefit from direct, personalized instruction.</div>
+				<div class="studentsbtmwrap"><a class="studentsbtm" href="https://itunes.apple.com/us/app/elite-instructors-student/id991247242?mt=8">Find an Instructor</a></div>
+			</div>
+		</div>
+		<div id="section_instructors" class="section_instructors">
+			<div class="pk">
+				<div class="instructorshalf">
+					<div class="instructorstext1">For</div>
+					<div class="instructorstext2">Instructors</div>
+					<div class="instructorstext3">Anyone with knowledge or expirience in a subject can tutor.<br/>The process is simple.</div>
+					<div class="instructorsbtmwrap"><a class="instructorsbtm" href="https://itunes.apple.com/us/app/elite-instructors-student/id991247242?mt=8">Become an Instructor</a></div>
+				</div>
+				<div class="instructorshalf"><img class="iphone-instructors" src="img/iPhone 6_mockup_instructors_white@2x.png"></div>
+			</div>
+			<div class="mobile">
+				<div class="instructorshalf"><img class="iphone-instructors" src="img/iPhone 6_mockup_instructors_white@2x.png"></div>
+				<div class="instructorshalf">
+					<div class="instructorstext1">For</div>
+					<div class="instructorstext2">Instructors</div>
+					<div class="instructorstext3">Anyone with knowledge or expirience in a subject can tutor.<br/>The process is simple.</div>
+					<div class="instructorsbtmwrap"><a class="instructorsbtm" href="https://itunes.apple.com/us/app/elite-instructors-student/id991247242?mt=8">Become an Instructor</a></div>
+				</div>
+			</div>
+		</div>
+		<div class="pk">
+			<div id="section_features" class="section_features">
+			<div class="featuresthreefive">
+				<div class="overflow"><img class="futureimgleft" src="img/play_button@2x.png"></div>
+				<div class="futuretitleleft">FEATURE ONE</div>
+				<div class="futuretextleft">Anyone with knowledge or experience in a subject can tutor. The process is simple.</div>
+				<div style="overflow: auto;"><img style="padding-top: 40px;" class="futureimgleft" src="img/play_button@2x.png"></div>
+				<div class="futuretitleleft">FEATURE TWO</div>
+				<div class="futuretextleft">Anyone with knowledge or experience in a subject can tutor. The process is simple.</div>
+			</div>
+			<div class="featuresthreezero"><img style="width:auto;height:500px;display:block;margin:0 auto;" src="img/iPhone6_mockup_features_black@2x.png"></div>
+			<div class="featuresthreefive">
+				<div style="overflow: auto;"><img class="futureimgright" src="img/play_button@2x.png"></div>
+				<div class="futuretitleright">FEATURE THREE</div>
+				<div class="futuretextright">Anyone with knowledge or experience in a subject can tutor. The process is simple.</div>
+				<div style="overflow: auto;"><img style="padding-top: 40px;" class="futureimgright" src="img/play_button@2x.png"></div>
+				<div class="futuretitleright">FEATURE FOUR</div>
+				<div class="futuretextright">Anyone with knowledge or experience in a subject can tutor. The process is simple.</div>
+			</div>
+			</div>
+		</div>
+	<div class="mobile">
+		<div id="section_features" class="section_features">
+			<div class="featuresthreezero"><img style="width:auto;height:500px;display:block;margin:0 auto;" src="img/iPhone6_mockup_features_black@2x.png"></div>
+			<div class="featuresthreefive">
+				<div class="overflow"><img class="futureimgleft" src="img/play_button@2x.png"></div>
+				<div class="futuretitleleft">FEATURE ONE</div>
+				<div class="futuretextleft">Anyone with knowledge or experience in a subject can tutor. The process is simple.</div>
+				<div style="overflow: auto;"><img style="padding-top: 40px;" class="futureimgleft" src="img/play_button@2x.png"></div>
+				<div class="futuretitleleft">FEATURE TWO</div>
+				<div class="futuretextleft">Anyone with knowledge or experience in a subject can tutor. The process is simple.</div>
+			</div>
+			<div class="featuresthreefive">
+				<div style="overflow: auto;"><img class="futureimgright" src="img/play_button@2x.png"></div>
+				<div class="futuretitleright">FEATURE THREE</div>
+				<div class="futuretextright">Anyone with knowledge or experience in a subject can tutor. The process is simple.</div>
+				<div style="overflow: auto;"><img style="padding-top: 40px;" class="futureimgright" src="img/play_button@2x.png"></div>
+				<div class="futuretitleright">FEATURE FOUR</div>
+				<div class="futuretextright">Anyone with knowledge or experience in a subject can tutor. The process is simple.</div>
+			</div>
+		</div>
+	</div>
+		<div class="pk">
+			<div id="videoitshelg2" style="text-align:center">
+				<iframe id="video2" width="100%;" src="https://www.youtube.com/embed/G_0l8nlnceE?enablejsapi=1" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<div id="closevideorightnow2"></div>
+		</div>
+	    <div class="mobile">
+			<div class="mobilevideo2">
+				<iframe class="videoiframemobile2"  src="https://www.youtube.com/embed/G_0l8nlnceE?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>
+		<div class="section_video">
+			<div class="pk"><button id="videobuttondawdaw2" class="videobuttondawdaw2"></button></div>
+			<div class="mobile"><button id="videobuttondawdaw12" class="videobuttondawdaw2" style="margin-top:20px;"></div>
+			<div class="watchvideotext">Watch the video </div>
+		</div>
+		<div id="section_download" class="section_download">
+			<div class="downloadtitle">Download Elite now</div>
+			<div><a href="https://itunes.apple.com/us/app/elite-instructors-student/id991247242?mt=8"><img class="appstorebtm2" src="img/app_store_button@2x.png"></a></div>
+			<div>
+				<form>
+				<div class="downloadfomtext">Stay Updated</div>
+					<div class="inputsborder">
+					<input style="" class="input-sign" type="text" placeholder="Your Email" name="email">
+					<input type="submit" class="popupsend" value="SUBSCRIBE" name="btnsend">
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="footer">
+			<div class="footerhalf"><div class="footercopytext">&copy; 2016 Elite Instructors, Inc. All rights reserved.</div></div>
+			<div class="footerhalf">
+				<div class="socialiconsfooter">
+				<div class="contact">Contact</div>
+					<a href="https://www.facebook.com/eliteapp/">
+					<img class="socialiconsimg" src="img/facebook@2x.png">
+					</a>
+					<a href="https://twitter.com/elite_app">
+					<img class="socialiconsimg" src="img/twitter@2x.png">
+					</a>
+					<a href="https://www.instagram.com/eliteapp/?hl=en">
+					<img class="socialiconsimg" src="img/Instagram@2x.png">
+					</a>
+					<a href="https://www.linkedin.com/company/elite---application">
+						<img class="socialiconsimg" src="img/linkedin@2x.png">
+					</a>
+				</div>
+			</div>
+		</div>
+
+<script type="text/javascript">
+
+	$(document).ready(function() {
+
+		$('#videobuttondawdaw').on('click', function(ev) {
+			var src="https://www.youtube.com/embed/G_0l8nlnceE?enablejsapi=1&autoplay=1"
+			$("#video1")[0].src = src;
+			//	player.playVideo();
+			$('#videobuttondawdaw').hide();
+			jQuery( "#videoitshelg" ).addClass( "displayblock" );
+			jQuery( "#closevideorightnow" ).addClass( "displayblock" );
+			ev.preventDefault();
+		});
+
+		$('#closevideorightnow').on('click', function(ev) {
+			var url = $('#video1').attr('src');
+			$('#video1').attr('src', '');
+			// $('#video1').attr('src', url);
+			$('#videobuttondawdaw').show();
+			jQuery( "#videoitshelg" ).removeClass( "displayblock" );
+			jQuery( "#closevideorightnow" ).removeClass( "displayblock" );
+		});
+	});
+</script>
+	<script type="text/javascript">
+		$('#videobuttondawdaw123123').click(function(){
+			$('.mobilevideo').show();
+			$('.videoiframemobile').attr('src', 'https://www.youtube.com/embed/G_0l8nlnceE?enablejsapi=1&autoplay=1');
+		});
+		$('.mobilevideo').click(function(){
+			$('.mobilevideo').hide();
+			$('.videoiframemobile').attr('src', '');
+		});
+	</script>
+	<script type="text/javascript">
+		$('#videobuttondawdaw12').click(function(){
+			$('.mobilevideo2').show();
+			$('.videoiframemobile2').attr('src', 'https://www.youtube.com/embed/G_0l8nlnceE?enablejsapi=1&autoplay=1');
+		});
+		$('.mobilevideo2').click(function(){
+			$('.mobilevideo2').hide();
+			$('.videoiframemobile2').attr('src', '');
+		});
+	</script>
+	<script type="text/javascript">
+
+		$(document).ready(function() {
+
+			$('#videobuttondawdaw2').on('click', function(ev) {
+				var src="https://www.youtube.com/embed/G_0l8nlnceE?enablejsapi=1&autoplay=1"
+				$("#video2")[0].src = src;
+				//	player.playVideo();
+				$('#videobuttondawdaw2').hide();
+				jQuery( "#videoitshelg2" ).addClass( "displayblock" );
+				jQuery( "#closevideorightnow2" ).addClass( "displayblock" );
+				ev.preventDefault();
+			});
+
+			$('#closevideorightnow2').on('click', function(ev) {
+				var url = $('#video2').attr('src');
+				$('#video2').attr('src', '');
+				// $('#video1').attr('src', url);
+				$('#videobuttondawdaw2').show();
+				jQuery( "#videoitshelg2" ).removeClass( "displayblock" );
+				jQuery( "#closevideorightnow2" ).removeClass( "displayblock" );
+			});
+		});
+	</script>
+
+			<script>
+			$(window).scroll(function() {
+				var scroll = $(window).scrollTop();
+				 //console.log(scroll);
+				if (scroll >= 50) {
+					//console.log('a');
+					$(".topmenu").addClass("menuunderscroll");
+				} else {
+					//console.log('a');
+					$(".topmenu").removeClass("menuunderscroll");
+				}
+			});
+			</script>
+			<script>
+
+			$(document).ready(function () {
+				$(document).on("scroll", onScroll);
+
+				//smoothscroll
+				$('a[href^="#"]').on('click', function (e) {
+					e.preventDefault();
+					$(document).off("scroll");
+
+					$('a').each(function () {
+						$(this).removeClass('activemenuitem');
+					})
+					$(this).addClass('activemenuitem');
+
+					var target = this.hash,
+						menu = target;
+					$target = $(target);
+					$('html, body').stop().animate({
+						'scrollTop': $target.offset().top+2
+					}, 500, 'swing', function () {
+						window.location.hash = target;
+						$(document).on("scroll", onScroll);
+					});
+				});
+			});
+
+			function onScroll(event){
+				var scrollPos = $(document).scrollTop();
+				$('.menuitemswrap a').each(function () {
+					var currLink = $(this);
+					var refElement = $(currLink.attr("href"));
+					if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+						$('.menuitemswrap a').removeClass("activemenuitem");
+						currLink.addClass("activemenuitem");
+					}
+					else{
+						currLink.removeClass("activemenuitem");
+					}
+				});
+				}
+			</script>
+
+		</body>
+</html>
